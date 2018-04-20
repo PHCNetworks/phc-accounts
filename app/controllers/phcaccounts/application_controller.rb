c1172c07402fa18150d5d4b266c2420c713312cc
BradPotts <brad.potts@phcnetworks.net>
2018-04-19 21:55:21 -0700

    updated views with various fixes addin user menu bar with gravatar.

--- app/controllers/phcaccounts/application_controller.rb
+++ app/controllers/phcaccounts/application_controller.rb
@@ -1,5 +1,24 @@
 module Phcaccounts
   class ApplicationController < ActionController::Base
+
+    # Devise Filter
+    before_action :phc_devise_permitted_parameters, if: :devise_controller?
+
+    # Filter and Security
     protect_from_forgery with: :exception
+
+    protected
+
+    #
+    def phc_devise_permitted_parameters
+      added_attrs = [:username, :firstname, :lastname, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+    # Load Helpers & Load Helpers to Mainapp
+    helper Phctitleseo::Engine.helpers
+    helper Phcnotifi::Engine.helpers
+
   end
 end
--- app/models/phcaccounts/user.rb
+++ app/models/phcaccounts/user.rb
@@ -1,8 +1,23 @@
 module Phcaccounts
   class User < ApplicationRecord
+
+    # Autogenerate Organization ID
+    before_create :phc_devise_generate_org_id
+
+    # User Gravatar Support
+    include Gravtastic
+    gravtastic
+
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+    private
+
+    # Autogenerate Organization ID
+    def phc_devise_generate_org_id
+      self.org_id = SecureRandom.hex(5)
+    end
+
   end
 end
