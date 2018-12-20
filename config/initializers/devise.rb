f8d4beac9a5a3f4ebfad0733737b5b46df5ef8b3
BradPotts <brad.potts@phcnetworks.net>
2018-12-20 12:17:07 +0000

    fixed devise intializer.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  config.parent_controller = 'phcaccounts::ApplicationController'
+  config.parent_controller = 'Phcaccounts::ApplicationController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
